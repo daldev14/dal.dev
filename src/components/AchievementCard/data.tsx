6303eabe1b6f00ee76e2016465fbf72fee904b5a
Daniel Amaya <damayalop@gmail.com>
2025-02-12 12:14:25 -0600

    Add AchievementCard component and update layout to include achievements section

---
+++ src/components/AchievementCard/data.tsx
@@ -0,0 +1,13 @@
+export const ACHIEVEMENT = [
+    {
+        title: "Bootcamp Desarrollador FullStack",
+        company: "UTH + Coursera",
+        description: "Completé intensivo programa de desarrollo web full-stack, dominando tecnologías modernas y mejores prácticas de la industria.",
+        href: "https://programateuth.vercel.app/"
+    },{
+        title: "Participación Hackathon",
+        company: "Alura LATAM",
+        description: "Participé en un hackatón organizado por Alura Latam, colaborando en el desarrollo de una solución innovadora para un desafío tecnológico.",
+        href: "https://drive.google.com/file/d/115ObS61nGjP19rxNVxHapNxZ1OslO1dh/view?usp=sharing"
+    },
+];
